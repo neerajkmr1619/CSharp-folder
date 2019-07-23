e46f5077b80928c7635d82752b9a72cf17d021e9
neerajkmr1619 <neerajkmr1619@gmail.com>
2019-07-23 08:47:18 +0530

    Adding project files

---
+++ LearnLinq.cs
@@ -0,0 +1,12 @@
+using System;
+namespace C_InterviewPrepration
+{
+    public class LearnLinq
+    {
+        public LearnLinq()
+        {
+            Console.WriteLine("hiiiii");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_InterviewPrepration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LearnLinq ll=new LearnLinq();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
